f62365d72839dab02e75b17b33e6f653df913256
tjswjd0218 <40467370+tjswjd0218@users.noreply.github.com>
2023-12-12 20:42:14 +0900

    [level unrated] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,11 +1,7 @@
 function solution(num_list) {
     let answer = [0, 0];
     for(item of num_list) {
-        if(item % 2 === 0){
-            answer[0] += 1     
-        } else {
-            answer[1] += 1
-        }
+        answer[item % 2] += 1
     }
     return answer;
 }
